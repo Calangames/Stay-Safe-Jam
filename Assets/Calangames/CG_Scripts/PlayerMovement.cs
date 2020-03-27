0df97ca61591e44dfdefde8681d16d5f98af7cf4
Lukas <50255971+LukasPizza@users.noreply.github.com>
2020-03-27 20:18:39 -0300

    assets

---
+++ Assets/Calangames/CG_Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float playerSpeed;
+    public float shipSpeed;
+    public float jumpForce;
+
+    public CharacterController playerController;
+
+    private Vector3 moveDirection;
+    public float gravityScale;
+    
+    void Start ()
+    {
+        playerController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        moveDirection = new Vector3(Input.GetAxis("Horizontal") * playerSpeed, moveDirection.y, Input.GetAxis("Vertical") * playerSpeed);
+
+        if (playerController.isGrounded)
+        {
+            moveDirection.y = 0f;
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDirection.y = jumpForce;
+            }
+        }
+        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime);
+        playerController.Move(moveDirection * Time.deltaTime);
+
+    }
+
+}
