3365482d885679fd0cc64c4f60d704a5e4bf8849
Lukas <50255971+LukasPizza@users.noreply.github.com>
2020-03-28 20:56:41 -0300

    Revert "Revert "lean tween platforms""
    This reverts commit c4d81a5221bb2d7b4cb3a75903fc5d05a8ed5440.

---
+++ Assets/LeanTweenManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeanTweenManager : MonoBehaviour
+{
+    public GameObject Mesh1;
+    public GameObject Mesh2;
+    public GameObject Mesh3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LeanTween.moveX(Mesh1, Mesh1.transform.position.x + 5f, 1f).setEase(LeanTweenType.easeInOutCirc).setLoopPingPong();
+        
+        LeanTween.rotateAround(Mesh2, Vector3.up, -360, 80f).setLoopClamp();
+        LeanTween.moveX(Mesh2, Mesh2.transform.position.x + 1f, 3f).setEase(LeanTweenType.easeInOutCirc).setLoopPingPong();
+
+        LeanTween.rotateAround(Mesh3, Vector3.up, -360, 80f).setLoopClamp();
+        LeanTween.moveY(Mesh3, Mesh3.transform.position.y + 5f, 3f).setEase(LeanTweenType.easeInOutCirc).setLoopPingPong();
+        LeanTween.moveX(Mesh3, Mesh3.transform.position.x + 5f, 3f).setEase(LeanTweenType.easeInOutCirc).setLoopPingPong();
+        LeanTween.scale(Mesh3, new Vector3(1.3922f, 1.273f, 1.07517507f), 1f).setLoopPingPong();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
