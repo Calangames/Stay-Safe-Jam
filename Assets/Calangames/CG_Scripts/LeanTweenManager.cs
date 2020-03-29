9cbc2a73eecd29a7fbe52caa970752429ea1d416
Lukas <50255971+LukasPizza@users.noreply.github.com>
2020-03-29 00:55:54 -0300

    it's britney bitch

--- Assets/Calangames/CG_Scripts/LeanTweenManager.cs
+++ Assets/Calangames/CG_Scripts/LeanTweenManager.cs
@@ -11,15 +11,15 @@ public class LeanTweenManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        LeanTween.moveX(Mesh1, Mesh1.transform.position.x + 5f, 1f).setEase(LeanTweenType.easeInOutCirc).setLoopPingPong();
+        LeanTween.moveX(Mesh1, Mesh1.transform.position.x + 20f, 1f).setLoopPingPong();
         
-        LeanTween.rotateAround(Mesh2, Vector3.up, -360, 80f).setLoopClamp();
-        LeanTween.moveX(Mesh2, Mesh2.transform.position.x + 1f, 3f).setEase(LeanTweenType.easeInOutCirc).setLoopPingPong();
+        LeanTween.rotateAround(Mesh2, Vector3.up, -360, 8f).setLoopClamp();
+        LeanTween.moveX(Mesh2, Mesh2.transform.position.x + 5f, 1f).setLoopPingPong();
 
-        LeanTween.rotateAround(Mesh3, Vector3.up, -360, 80f).setLoopClamp();
-        LeanTween.moveY(Mesh3, Mesh3.transform.position.y + 5f, 3f).setEase(LeanTweenType.easeInOutCirc).setLoopPingPong();
-        LeanTween.moveX(Mesh3, Mesh3.transform.position.x + 5f, 3f).setEase(LeanTweenType.easeInOutCirc).setLoopPingPong();
-        LeanTween.scale(Mesh3, new Vector3(1.3922f, 1.273f, 1.07517507f), 1f).setLoopPingPong();
+        LeanTween.rotateAround(Mesh3, Vector3.up, -360, 25f).setLoopClamp();
+        LeanTween.moveY(Mesh3, Mesh3.transform.position.y - 10f, 1f).setEase(LeanTweenType.easeInOutCirc).setLoopPingPong();
+        LeanTween.moveX(Mesh3, Mesh3.transform.position.x + 5f, 1f).setEase(LeanTweenType.easeInOutCirc).setLoopPingPong();
+        LeanTween.scale(Mesh3, new Vector3(24f, 4f, 47f), 0.5f).setLoopPingPong();
 
     }
 
